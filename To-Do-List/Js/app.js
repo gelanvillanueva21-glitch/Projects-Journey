9117120179975ef291dfcc094adec5f6e015583a
Gelan <gelanvillanueva21@gmail.com>
2026-06-30 00:09:37 +0800

    Almost finished the frontend design, it is vibecoded, cuz I hate designing only js I manually do but still I understand 99% of the css and html

---
+++ To-Do-List/Js/app.js
@@ -0,0 +1,62 @@
+
+
+// VARIABLES
+
+const checkBox = document.getElementById('show-password-checkbox');
+const password = document.getElementById('password');
+const addButton = document.getElementById('add-btn');
+const addInputList = document.getElementById('to-do-list-title');
+const toDoList = document.querySelector('.list-box ul');
+const notiWindow = document.querySelector('.popup-notification');
+
+
+// SHOW PASSWORD LAYER
+
+checkBox.addEventListener('change', () => {
+
+    if (checkBox.checked) {
+        password.type = 'text';
+    } else {
+        password.type = 'password';
+    }
+
+});
+
+
+// ADD LIST LAYER 
+
+addButton.addEventListener('click', () => {
+
+    if (addInputList.value) {
+        const allList = document.querySelectorAll('.list-box ul li.list');
+        let isAlreadyExist = false;
+        allList.forEach((list) => {
+            if (list.innerText === addInputList.value) {
+                isAlreadyExist = true
+                return
+            }
+        })
+        if (!isAlreadyExist) {
+            let li = document.createElement('li');
+            li.classList.add('list');
+            let btn = document.createElement('button');
+            btn.classList.add('trash-btn');
+            let img = document.createElement('img');
+            img.src = '../Images/trash.svg';
+            btn.appendChild(img);
+            img.classList.add('image');
+            li.innerText = addInputList.value
+            li.appendChild(btn)
+            toDoList.appendChild(li);
+            notiWindow.style.display = 'block';
+        }
+    }
+});
+
+window.addEventListener('click', (e) => {
+
+    if (event.target.classList.contains('popup-notification')) {
+        notiWindow.style.display = 'none';
+    }
+
+})
