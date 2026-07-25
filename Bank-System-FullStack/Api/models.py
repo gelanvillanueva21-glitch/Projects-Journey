e401db975c3796ad5ab15711dddf00b22d928d69
Gelan <gelanvillanueva21@gmail.com>
2026-07-25 11:40:20 +0800

    Temporarily Finished the <[schemas.py & models.py]> at <[Bank-System-FullStack]> maybe sooner gotta change something there to adjust in the algorithm of the api

--- Bank-System-FullStack/Api/models.py
+++ Bank-System-FullStack/Api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey, func, DateTime
+from sqlalchemy import String, ForeignKey, func, DateTime, DECIMAL
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from database import Base
@@ -13,13 +13,19 @@ class User(Base):
     hashed_password : Mapped[str] = mapped_column(String(150))
     name : Mapped[str] = mapped_column(String(155))
     is_active : Mapped[bool] = mapped_column(default = True)
+    can_lend : Mapped[bool] = mapped_column(default = False)
+    monthly_payment_amount : Mapped[DECIMAL] = mapped_column(default = 0.00)
+    anual_interest_rate : Mapped[int] = mapped_column(default = 0)
+    amount_lend : Mapped[int] = mapped_column(default = 0)
     created_at : Mapped[datetime] = mapped_column(
         DateTime(timezone = True), 
         server_default = func.now())
-    money : Mapped[int] = mapped_column(default = 0)
+    availabe_balance : Mapped[int] = mapped_column(default = 0)
     
     loans : Mapped[list["Loan"]] = relationship(back_populates = "owner")
     withdraws : Mapped[list["Withdraw"]] = relationship(back_populates = "owner")
+    deposits : Mapped[list["Deposit"]] = relationship(back_populates = "owner")
+    loan_payment_history = Mapped[list["LoanPayment"]] = relationship(back_populates = "owner")
 
 
 
@@ -27,14 +33,32 @@ class Loan(Base):
     __tablename__ = "loans"
     
     id : Mapped[int] = mapped_column(primary_key = True)
-    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"))
-    loan_name : Mapped[str] = mapped_column(String(155))
-    date : Mapped[datetime] = mapped_column(
+    debtor_id : Mapped[int]
+    lender_id : Mapped[int]
+    loaned_date : Mapped[datetime] = mapped_column(
         DateTime(timezone = False),
         server_default = func.now())
-    laon_value : Mapped[datetime]
+    due_date : Mapped[datetime | None] = mapped_column(default = None)
+    monthly_due_date : Mapped[datetime | None] = mapped_column(default = None)
+    loan_balance : Mapped[int]
+    anual_interest_rate : Mapped[int]
     
     owner : Mapped["User"] = relationship(back_populates = "loans")
+
+
+
+class LoanPayment(Base):
+    __tablename__ = "loanpayment"
+    
+    id : Mapped[int] = mapped_column(primary_key = True)
+    deptor_paid_id : Mapped[int]
+    lender_id : Mapped[int]
+    paid_date : Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now())
+    paid_amount : Mapped[int]
+    
+    owner : Mapped["User"] = relationship(back_populates = "loan_payment_history")
 
 
 
@@ -46,9 +70,25 @@ class Withdraw(Base):
     date : Mapped[datetime] = mapped_column(
         DateTime(timezone = True),
         server_default = func.now())
-    money : Mapped[int]
+    withdraw_balance : Mapped[int]
     
     owner : Mapped["User"] = mapped_column(relationship(back_populates = "withdraws"))
+
+
+
+class Deposit(Base):
+    __tablename__ = "deposits"
+    
+    id : Mapped[int] = mapped_column(primary_key = True)
+    user_id : Mapped[int]
+    date : Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now())
+    deposit_balance : Mapped[int]
+    
+    owner = Mapped["User"] = relationship(back_populates = "deposits")
+
+
 
 
 
--- Bank-System-FullStack/Api/schemas.py
+++ Bank-System-FullStack/Api/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from typing import Annotated
+from datetime import datetime
 
 
 
@@ -12,12 +13,48 @@ class CreateUser(UserBase):
     password : Annotated[str, Field(min_length = 8, max_length = 50)]
 
 
+
 class UserResponse(BaseModel):
     id : int
     email : EmailStr
     is_active : Annotated[bool, Field(default = True)]
     
     model_config = ConfigDict(from_attributes = True)
+
+
+
+
+class Laon(BaseModel):
+    deptor_id : int
+    lender_id : int
+    loan_value : int
+    anual_interest_rate : int
+    monthly_due_date : datetime | None
+    due_date : datetime | None
+
+
+
+class LoanRespone(BaseModel):
+    loan_value : int
+    anual_interest_rate : int
+    monthly_due_date : datetime | None
+    due_date : datetime | None
+    
+    model_config = ConfigDict(from_attributes = True)
+
+
+
+class Withdraw(BaseModel):
+    id : int
+    withdraw_amount : int
+
+
+
+class Deposit(BaseModel):
+    id : int
+    deposit_amount : int
+
+
 
 
 
