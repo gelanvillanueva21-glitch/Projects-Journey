5b7fc4274943daf8cc34efe827bf0c8b8f8fa319
Gelan <gelanvillanueva21@gmail.com>
2026-08-03 21:21:58 +0800

    Modify <[user_directory.tsx]>

--- Project-Based-Learning/TypeScript/React-Mini-Project/user_directory.tsx
+++ Project-Based-Learning/TypeScript/React-Mini-Project/user_directory.tsx
@@ -26,11 +26,13 @@ function UserDirectory({ users, isLoading, error}: UserDirectoryProps): React.JS
     if (users.length === 0) return <p>No users found.</p>
     return (
         <ul>
-            {users.map((user, index) => (
+            {users.map((user) => (
                 <li 
                 key={user.id} 
                 className={user.isActive? 
-                    "active" : "inactive"}></li>
+                    "active" : "inactive"}>
+                        {user.name}-{user.role}
+                </li>
             ))}
         </ul>
     )
