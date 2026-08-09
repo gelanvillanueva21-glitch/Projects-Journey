a8557b61fa525cf9eb755e909f648ae07494900a
Gelan Villanueva <gelanvillanueva21@gmail.com>
2026-08-09 17:40:36 +0800

    Learned <[UseReft.tsx, useEffects.tsx]> properly

--- Project-Based-Learning/TypeScript/React-Mini-Project/React/src/main.tsx
+++ Project-Based-Learning/TypeScript/React-Mini-Project/React/src/main.tsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client"
-import CallBack, {MemoCallBack} from "./useCallBack";
+import UseRef from "./useRef";
 import "./index.css"
 
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <MemoCallBack/>
+        <UseRef/>
     </React.StrictMode>
 )
 
