d40c59e1aa9eca13c88a6c5f104c8946f952a8fa
Gelan Villanueva <gelanvillanueva21@gmail.com>
2026-08-16 19:27:55 +0800

    Finished Authentication Layer

---
+++ Taskflow/backend/app/repositories/user.py
@@ -0,0 +1,41 @@
+
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import hash_password
+
+
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.db.execute(select(User).where(User.email == email))
+        return result.scalar_one_or_none()
+
+
+    async def get_by_id(self,user_id: int) -> User | None:
+        result = await self.db.execute(select(User).where(User.id == id))
+        return result.scalar_one_or_none()
+
+
+    async def create(self, data: UserCreate) -> User:
+        user = User(
+            email=data.email,
+            hashed_password=hash_password(data.password),
+            full_name=data.full_name
+        )
+        self.db.add(user)
+        await self.db.commit()
+        await self.db.refresh(user)
+        return user
+
+
+
+
+
