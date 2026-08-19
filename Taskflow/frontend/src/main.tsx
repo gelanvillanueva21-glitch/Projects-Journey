c848abfa7c01a90f3f59401712ac9db540bb4a20
Gelan Villanueva <gelanvillanueva21@gmail.com>
2026-08-19 23:39:10 +0800

    Certify Finished <[Taskflow]>

--- Taskflow/frontend/src/main.tsx
+++ Taskflow/frontend/src/main.tsx
@@ -3,15 +3,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import { AuthProvider } from "./context/AuthContext";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import App from "./App";
 import "./index.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+        <ErrorBoundary>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </ErrorBoundary>
     </React.StrictMode>,
 )
 
